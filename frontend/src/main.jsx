4c4239e064b6484503d95a1bf3cb1afea2160d04
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-02-06 10:59:34 +0600

    Updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,18 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
+import Profile from "./pages/User/Profile.jsx";
+
+//private route
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Route>
