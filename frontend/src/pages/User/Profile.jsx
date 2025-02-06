b35e73c9b40ebbd20b6cf71d1823964b7f952fc8
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-02-06 11:37:43 +0600

    Updated

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { Link } from "react-router-dom";
+import { useProfileMutation } from "../../redux/api/usersApiSlice";
+
 const Profile = () => {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { userInfo } = useSelector((state) => state.auth);
+  const [updateProfile, { isLoading: loadingUpadteProfile }] =
+    useProfileMutation();
+
+  useEffect(() => {
+    setUsername(userInfo.username);
+    setEmail(userInfo.email);
+  }, [userInfo.email, userInfo.username]);
+
+  const dispatch = useDispatch();
+  return (
+    <div className="container mx-auto p-4 mt-[10rem]">
+      <div className="flex justify-center align-center md:flex md:space-x-4">
+        <div className="md:w-1/3">
+          <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+          <form action="">
+            <div className="mb-4">
+              <label className="block text-black mb-2">Name</label>
+              <input
+                type="text"
+                placeholder="Enter Name"
+                className="form-input p-4 rounded-sm w-full"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
