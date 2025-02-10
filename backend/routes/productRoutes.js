6575b38a5afb9616116ad928c769c9d763074fb6
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-02-10 13:38:09 +0600

    Small Bug fixed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 //Controller
 import { addProduct } from "../controllers/productController.js";
-
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
+
 router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
 
 export default router;
