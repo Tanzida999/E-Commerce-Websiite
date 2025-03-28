52a9ff09c07cf6760ba8c52107304d49a247714a
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-03-28 23:25:21 +0600

    product details Updated

--- frontend/src/pages/Products/ProductDetails.jsx
+++ frontend/src/pages/Products/ProductDetails.jsx
@@ -48,6 +48,68 @@ const ProductDetails = () => {
           Go Back
         </Link>
       </div>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.message}
+        </Message>
+      ) : (
+        <>
+          <div className="flex flex-wrap  items-between mt-[2rem] ml-[10rem]">
+            <div className="flex-shrink-0 mr-[2rem]">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full xl:w-[50rem] lg:w-[45rem] md:w-[30rem] sm:w-[20rem] mr-[2rem]"
+              />
+
+              <HeartIcon product={product} />
+            </div>
+            <div className="flex flex-col justify-between">
+              <h2 className="text-2xl font-semibold">{product.name}</h2>
+              <p className="my-2 xl:w-[35rem] lg:w-[45rem] md:w-[30rem] text-[#B0B0B0]">
+                {product.description}{" "}
+              </p>
+              <p className="text-3xl my-2 font-extrabold">${product.price}</p>
+              <div className="flex items center justify-between w-[20rem]">
+                <div className="one">
+                  <h1 className="flex items-center mb-6">
+                    <FaStore className="mr-2 text-black " />
+                    Brand: {product.brand}
+                  </h1>
+                  <h1 className="flex items-center mb-6 w-[10rem]">
+                    <FaClock className="mr-2 text-black " />
+                    Added:{" "}
+                    {product.createdAt
+                      ? moment(product.createdAt).fromNow()
+                      : "N/A"}
+                  </h1>
+                  <h1 className="flex items-center mb-6">
+                    <FaStar className="mr-2 text-black " />
+                    Reviews: {product.numReviews}
+                  </h1>
+                </div>
+
+                <div className="two">
+                  <h1 className="flex items-center mb-6">
+                    <FaStar className="mr-2 text-white" />
+                    Rating: {rating}
+                  </h1>
+                  <h1 className="flex items-center mb-6">
+                    <FaShoppingCart className="mr-2 text-white" />
+                    Quantity: {product.quantity}
+                  </h1>
+                  <h1 className="flex items-center mb-6">
+                    <FaBox className="mr-2 text-white" />
+                    In Stock: {product.countInStock}
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
