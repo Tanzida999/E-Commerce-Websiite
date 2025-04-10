1835d02d60eafae419691bef14f3744f2e4bac78
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-04-10 22:31:58 +0600

    Shop started

---
+++ frontend/src/redux/features/shop/shopSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categories: [],
+  products: [],
+  checked: [],
+  radio: [],
+  brandCheckboxes: {},
+  checkedBrands: [],
+};
+const shopSlice = createSlice({
+  name: "shop",
+  initialState,
+  reducers: {
+    setCategories: (state, action) => {
+      state.categories = action.payload;
+    },
+  },
+});
