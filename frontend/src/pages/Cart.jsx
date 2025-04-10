24ac87d80a498b7be119f1f019ab4bd6798217bd
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-04-10 21:40:29 +0600

    Almost done with cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -12,6 +12,13 @@ const Cart = () => {
   const addToCartHandler = (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
   };
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
+  };
   return (
     <>
       <div className="container flex justify-around items-start wrap mx-auto mt-8">
@@ -53,15 +60,51 @@ const Cart = () => {
                         addToCartHandler(item, Number(e.target.value))
                       }
                     >
-                      {[...Array(item.countInStock).keys()].map((x) => {
+                      {[...Array(item.countInStock).keys()].map((x) => (
                         <option key={x + 1} value={x + 1}>
                           {x + 1}
-                        </option>;
-                      })}
+                        </option>
+                      ))}
                     </select>
+                  </div>
+
+                  <div
+                    className="
+                  "
+                  >
+                    <button
+                      className="text-red-500 mr-[5rem]"
+                      onClick={() => {
+                        removeFromCartHandler(item._id);
+                      }}
+                    >
+                      <FaTrash className="ml-[1rem]" />
+                    </button>
                   </div>
                 </div>
               ))}
+
+              <div className="mt-8 w-[40rem]">
+                <div className="p-4 rounded-lg">
+                  <h2 className="text-xl font-semibold mb-2">
+                    Items ({cartItems.reduce((acc, item) => acc + item.qty, 0)}{" "}
+                    )
+                  </h2>
+                  <div className="text-2xl font-bold">
+                    $
+                    {cartItems
+                      .reduce((acc, item) => acc + item.qty * item.price, 0)
+                      .toFixed(2)}
+                  </div>
+                  <button
+                    className="bg-pink-500 mt-4 py-2 px-4 rounded-full text-lg w-full"
+                    disabled={cartItems.length === 0}
+                    onClick={checkoutHandler}
+                  >
+                    Proccess To Checkout
+                  </button>
+                </div>
+              </div>
             </div>
           </>
         )}
